bc09d984e0ba6ed7698aa8a3443017564b86fe0c
DraganSimic98 <dragansimic29@gmail.com>
2024-01-06 15:25:13 +0100

    LogIn LogOut

--- AppointmentScheduler/Startup.cs
+++ AppointmentScheduler/Startup.cs
@@ -2,6 +2,7 @@ using AppointmentScheduler.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +30,8 @@ namespace AppointmentScheduler
               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
           );
             services.AddControllersWithViews();
+
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +53,7 @@ namespace AppointmentScheduler
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
