c0cdc2631c81331be824cda7ff2fdbf705b56967
DraganSimic98 <dragansimic29@gmail.com>
2024-01-06 13:43:54 +0100

    RegisterViewSetUp

---
+++ AppointmentScheduler/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppointmentScheduler.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The{0} must be at least {2} characters long", MinimumLength = 6)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The Password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+}
