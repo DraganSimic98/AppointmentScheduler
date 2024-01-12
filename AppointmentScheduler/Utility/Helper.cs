c69af517a5c7f082755ed35209060b326f5328de
DraganSimic98 <dragansimic29@gmail.com>
2024-01-12 23:36:35 +0100

    Work on partialView (add buttons)

--- AppointmentScheduler/Utility/Helper.cs
+++ AppointmentScheduler/Utility/Helper.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 
 namespace AppointmentScheduler.Utility
 {
@@ -18,6 +19,20 @@ namespace AppointmentScheduler.Utility
                 new SelectListItem{ Value=Helper.Doctor, Text=Helper.Doctor }
 
             };
+        }
+
+        public static List<SelectListItem> GetTimeDropDown() 
+        {
+            int minute = 60;
+            List<SelectListItem> duration = new List<SelectListItem>();
+            for(int i = 1; i <= 12;i++)
+            {
+                duration.Add(new SelectListItem { Value = minute.ToString(), Text=i + "Hr"});
+                minute = minute + 30;
+                duration.Add(new SelectListItem { Value = minute.ToString(), Text = i + "Hr 30 min" });
+                minute = minute + 30;
+            }
+            return duration;
         }
     }
 }
