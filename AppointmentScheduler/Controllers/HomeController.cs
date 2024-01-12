a06259b653c145800c085b34971818826b184791
DraganSimic98 <dragansimic29@gmail.com>
2024-01-12 22:02:19 +0100

    ovo je neki novi komit

--- AppointmentScheduler/Controllers/HomeController.cs
+++ AppointmentScheduler/Controllers/HomeController.cs
@@ -35,7 +35,6 @@ namespace AppointmentScheduler.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         { 
-
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
