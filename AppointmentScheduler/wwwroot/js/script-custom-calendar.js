5d11f123ff432cc01cb7189f2119aa06de3cf62d
DraganSimic98 <dragansimic29@gmail.com>
2024-01-13 18:59:31 +0100

    Build

--- AppointmentScheduler/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduler/wwwroot/js/script-custom-calendar.js
@@ -1,4 +1,12 @@
+/*const { data } = require("jquery");*/
+
+var routeUrl = location.protocol + "//" + location.host;
 $(document).ready(function () {
+    $("#appointmentDate").kendoDateTimePicker({
+        value: new Date(),
+        dateInput: false
+    });
+
     InitializeCalendar();
 });
 
@@ -36,4 +44,59 @@ function onShowModal(obj, isEventDetail) {
 
 function onCloseModal() {
     $("#appointmentInput").modal("hide");
+}
+
+function onSubmitForm() {
+    if (chechValidateion()) {
+
+        var requestData = {
+            Id: parseInt($("#id").val()),
+            Title: $("#title").val(),
+            Description: $("#descritpion").val(),
+            StartDate: $("#appointmentDate").val(),
+            Duration: $("#duration").val(),
+            DoctorId: $("#doctorId").val(),
+            PatientId: $("#patientId").val(),
+        };
+
+        $.ajax({
+            url: routeUrl + '/api/Appointment/SaveCalendarData',
+            type: 'POST',
+            data: JSON.stringify(requestData),
+            contentType: 'application/json',
+            success: function (response) {
+                if (response.status === 1 || response.status === 2) {
+                    $.notify(response.message, "success");
+                    onCloseModal();
+                }
+                else {
+                    $.notify(response.message, "error");
+                }
+            },
+            error: function (xhr) {
+                $.notify("Error", "error");
+            }
+        });
+    }
+}
+
+function chechValidateion() {
+    var isValid = true;
+    if ($("#title").val() === undefined || $("#title").val() === "") {
+        isValid == false;
+        $("#title").addClass('error');
+    } else {
+        $("#title").removeClass('error');
+
+    }
+
+    var isValid = true;
+    if ($("#appointmentDate").val() === undefined || $("#appointmentDate").val() === "") {
+        isValid == false;
+        $("#appointmentDate").addClass('error');
+    } else {
+        $("#appointmentDate").removeClass('error');
+
+    }
+    return isValid;
 }
